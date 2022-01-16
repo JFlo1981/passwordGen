f4f7ecab9670c582ab782328654ec779ad70e226
JFlo1981 <jarod.florence@outlook.com>
2022-01-16 13:57:31 -0600

    new method to eliminate heavy if/else code section - array declarations no longer needed

--- Assets/script.js
+++ Assets/script.js
@@ -1,46 +1,37 @@
 // Assignment code here
 
-// i will create arrays to contain each charType and declare a var for them out of the function
-// upperCase array
-const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ];
-
-// lowerCase array
-const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-// numbers array
-const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-// spChar array
-const spChar = ['#', '$', '^', ')', '@', '%', '+', '/', "'", '?', ':', ',', '!', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+// i will create arrays to contain each charType and declare a var for them out of the function --- arrays no longer needed here
 
 // var decalred
-var selection = [];
+let selection = [];
 
 // start generatePassword function
 function generatePassword() {
 
-// i will prompt the user to select a password length and i will create var for the password length
-var userNumber;
-
-do {
-    userNumber = prompt("Choose a password length: (8-128 character)");
-    userNumber = Number(userNumber);
-}
-while (userNumber < 8 || userNumber > 128 || isNaN(userNumber)) {
-
-alert("Your password will be " + userNumber + " characters long.");
-    }
+   // i will prompt the user to select a password length and i will create var for the password length
+   let userNumber;
+   
+   do {
+       userNumber = prompt("Choose a password length: (8-128 character)");
+       userNumber = Number(userNumber);
+   }
+   while (userNumber < 8 || userNumber > 128 || isNaN(userNumber)) {
+   
+   alert("Your password will be " + userNumber + " characters long.");
+       }
 
 
 // i will prompt the user to select character types
-//  and create vars for character arrays
-//  added function to return to prompt if no character values are chosen
+// and create vars for character arrays --- character arrays no longer used
+// added function to return to prompt if no character values are chosen
+// added push to eliminate the heavy if and else if code section
+
 ask(); 
 function ask () {
     const yesUpper = confirm("Do you want UPPERCASE characters?");
     if (yesUpper) {
       alert("Your password will contain UPPERCASE characaters");
-      
+      selection.push('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
     }
     if (!yesUpper) {
       alert("Your password will not contain UPPERCASE characaters");
@@ -49,6 +40,7 @@ function ask () {
     const yesLower = confirm("Do you want lowercase characters?");
     if (yesLower) {
       alert("Your password will contain lowercase characaters");
+      selection.push('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
     }
     if (!yesLower) {
       alert("Your password will not contain lowercase characaters");
@@ -57,7 +49,8 @@ function ask () {
     const yesNumber = confirm("Do you want numeric characters?");
     if (yesNumber) {
       alert("Your password will contain numeric characaters");
-    }
+      selection.push('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+     }
     if (!yesNumber) {
       alert("Your password will not contain numeric characaters");
       }
@@ -65,80 +58,29 @@ function ask () {
     const yesSpecial = confirm("Do you want special characters (@, %, & etc)?");
     if (yesSpecial) {
       alert("Your password will contain special characaters");
+      selection.push('#', '$', '^', ')', '@', '%', '+', '/', "'", '?', ':', ',', '!', '(', '}', '{', ']', '[', '~', '-', '_', '.') ;
     }
     if (!yesSpecial) {
       alert("Your password will not contain special characaters");
       }
 
 
-// i will concat the arrays based on user choice and use if/else to select the correct array
+// i will concat the arrays based on user choice and use if/else to select the correct array -- if/else was ommited with the array.push method
 // if all no
     if (!yesLower && !yesUpper && !yesNumber && !yesSpecial) {
       prompt = alert("You must choose at least one character style!");
       ask();
     }
+};
 
-
-
-// if all yes
-    if (yesLower && yesUpper && yesNumber && yesSpecial) {
-      selection = spChar.concat(numbers, upperCase, lowerCase);
-    }
-// else if for 3 selections
-    else if (yesLower && yesUpper && yesNumber) {
-    selection = lowerCase.concat(numbers, upperCase);
-
-  } else if (yesLower && yesUpper && yesSpecial) {
-    selection = lowerCase.concat(spChar, upperCase);
-
-  } else if (yesLower && yesSpecial && yesNumber) {
-    selection = lowerCase.concat(numbers, spChar);
-
-  } else if (yesSpecial && yesUpper && yesNumber) {
-    selection = spChar.concat(numbers, upperCase);
-  }
-
-  // else if for 2 selection
-   else if (yesSpecial && yesNumber) {
-     selection = spChar.concat(numbers);
-
-  } else if (yesSpecial && yesLower) {
-     selection = spChar.concat(lowerCase);
-
-  } else if (yesSpecial && yesUpper) {
-     selection = spChar.concat(upperCase);
-
-  } else if (yesLower && yesNumber) {
-     selection = lowerCase.concat(numbers);
-
-  } else if (yesLower && yesUpper) {
-     selection = lowerCase.concat(upperCase);
-
-  } else if (yesNumber && yesUpper) {
-     selection = numbers.concat(upperCase);
-  } 
-
-  // else if for one selection
-    else if (yesLower) {
-      selection = lowerCase;
-
-  } else if (yesUpper) {
-      selection = upperCase;
-
-  } else if (yesNumber) {
-      selection = numbers;
-
-  } else if (yesSpecial) {
-      selection = spChar;
-  }
-}
-// i will pull random selections from the user selected concat array for the length of password selected by the user
+// i will pull random selections from the user selected concat. array for the length of password selected by the user
     let randomize = [];
     for (let i =0; i < userNumber; i++) {
       randomize += selection[Math.floor(Math.random() * selection.length)]
     }
     return randomize;
 };
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -148,7 +90,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 };
 
 // Add event listener to generate button
