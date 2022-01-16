181b395e7a3f8ce2937bd2c79a564150daeacd59
JFlo1981 <jarod.florence@outlook.com>
2022-01-15 18:20:40 -0600

    arrays and var created for selections

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,25 @@
 // Assignment code here
+
+// i will create arrays to contain each charType and declare a var for them out of the function
+// upperCase array
+const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ];
+
+// lowerCase array
+const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// numbers array
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// spChar array
+const spChar = ['#', '$', '^', ')', '@', '%', '+', '/', "'", '?', ':', ',', '!', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+
+// var decalred
+var selection;
+
+
 // i will prompt the user to select a password length
 // i will prompt the user to select character types
-// i will create arrays to contain each charType
+
 // i will create variables for the password length and character arrays
 // i will concat the arrays based on user choice and use if/else to select the correct array
 // i will pull random selections from the user selected concat array for the length of password selected by the user
